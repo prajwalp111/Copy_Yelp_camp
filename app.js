c71c755f2720be06c5a29aa2a80a89347b1ab979
Prajwalp111 <prajwalp1812@gmail.com>
2025-05-20 00:02:54 +0530

    some updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const CampGround = require('./models/campGround');
 
@@ -13,8 +14,11 @@ db.once('open',()=>{
     console.log('Database connection established')
 });
 
+app.engine('ejs',ejsMate);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 
