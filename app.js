6d388f64abf6c710e6a72dd2f23b763cdbe7c395
Prajwalp111 <prajwalp1812@gmail.com>
2025-05-19 21:32:17 +0530

    small changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
 db.once('open',()=>{
-    console.log('Database connection')
+    console.log('Database connection established')
 });
 
 app.set('view engine', 'ejs');
