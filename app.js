50a9214c485fbe872ef1704a3a496ed1538ba7f6
Prajwalp111 <prajwalp1812@gmail.com>
2025-05-27 00:39:52 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
-
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
-const CampGround = require('./models/campGround');
+const CampGround = require('./models/campGround')
+const ExpressError = require('./utils/ExpressError')
+const catchAsync = require('./utils/catchAsync')
+const Joi = require('joi')
+const { campgroundSchema } = require('./schemas')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -19,8 +22,7 @@ app.engine('ejs',ejsMate);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res)=>{
@@ -36,32 +38,58 @@ app.get('/campgrounds/new', (req, res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async(req, res)=>{
+const validationCamp = (req, res, next)=>{
+    
+
+    const { error } = campgroundSchema.validate(req.body);
+
+    if (error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(msg, 400);  // Assuming you are using a custom ExpressError class
+    }else{
+        next()
+    }
+
+}
+
+//\    if (!req.body.campground) throw new Error('No campground submitted');
+app.post('/campgrounds', validationCamp,catchAsync(async (req, res) => {
+    
     const newcamp = new CampGround(req.body.campground);
     await newcamp.save();
-    res.redirect(`/campgrounds/${newcamp._id}`)
-})
+    res.redirect(`/campgrounds/${newcamp._id}`);
+}));
 
-app.get('/campgrounds/:id',async (req, res)=>{
+app.get('/campgrounds/:id',catchAsync(async (req, res)=>{
     const campground = await CampGround.findById(req.params.id)
     res.render('campgrounds/show', {campground});
-})
+}))
 
-app.get('/campgrounds/:id/edit',async (req, res)=>{
+app.get('/campgrounds/:id/edit',catchAsync(async (req, res)=>{
     const campground = await CampGround.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-})
+}))
 
-app.put('/campgrounds/:id', async(req,res)=>{
+app.put('/campgrounds/:id', validationCamp, catchAsync(async(req,res)=>{
     const { id } = req.params;
     const campground =await CampGround.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req,res)=>{
+app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
     const { id } = req.params;
     await CampGround.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.all(/(.*)/,(req, res, next)=>{
+    next(new ExpressError('NOT FOUND', 404))
+})
+
+app.use((err, req, res, next)=>{
+    const { statusCode = 500} =err;
+    if(!err.message) err.message = 'somrthing went wrong..... e..r...r...o...r...';
+    res.status(statusCode).render('error',{ err });    
 })
 
 app.listen(5050,()=>{
--- seeds/index.js
+++ seeds/index.js
@@ -29,4 +29,6 @@ const seedDB = async ()=>{
     }
 }
 
-seedDB()
+seedDB().then(() => {
+    mongoose.connection.close();
+})
