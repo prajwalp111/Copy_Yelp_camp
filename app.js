b0cc934005a660c830d82f0df6605311c7767e29
Prajwalp111 <prajwalp1812@gmail.com>
2025-06-17 23:07:38 +0530

    some basic security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+ // require('dotenv').config();
 
 const express = require('express')
 const app = express();
@@ -22,12 +23,19 @@ const User = require('./models/user')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 
+const helmet = require('helmet')
+
+const sanitizeV5 = require('./utils/mongoSanitizeV5.js');
+app.set('query parser', 'extended');
+
 const sessionConfig = {
+    name : 'session',
     secret : 'thisisaverydangerousplace',
     resave : false,
     saveUninitialized : true,
     cookie : {
         httpOnly : true,
+        //safe : true,
         expire : Date.now() + 1000 * 60 * 60 * 7 * 24,
         maxAge : 1000 * 60 * 60 * 7 *24
 
@@ -46,8 +54,62 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 //app.use(express.static('public'))
-
+app.use(sanitizeV5({ replaceWith: '_' }));
 app.use(session(sessionConfig))
+
+app.use(helmet({contentSecurityPolicy:false}));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.tiles.mapbox.com/",
+    // "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.mapbox.com/",
+    // "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const connectSrcUrls = [
+    // "https://api.mapbox.com/",
+    // "https://a.tiles.mapbox.com/",
+    // "https://b.tiles.mapbox.com/",
+    // "https://events.mapbox.com/",
+    "https://api.maptiler.com/", // add this
+];
+
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dv2fw7wkz/",
+                "https://images.unsplash.com/",
+                "https://img.freepik.com/" ,
+                "https://api.maptiler.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 
 app.use(flash());
